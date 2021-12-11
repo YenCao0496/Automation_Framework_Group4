c94361f45b46dd2a75fc49b5f5b9496490670f54
mienbui1 <btmientest@gmail.com>
2021-12-11 17:55:07 +0700

    Update capture report by browser

--- src/test/java/core/driver/WebDriverFactory.java
+++ src/test/java/core/driver/WebDriverFactory.java
@@ -1,6 +1,8 @@
 package core.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,70 +16,85 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import core.utilities.EvnConfig;
 
 import java.net.URL;
+
 public class WebDriverFactory {
 	protected WebDriver webDriver;
 
-    public WebDriverFactory() {
-        this.webDriver = setWebDriver();
-    }
+	public WebDriverFactory() {
+		this.webDriver = setWebDriver();
+	}
 
-    public WebDriver getDriver() {
-        return this.webDriver;
-    }
+	public WebDriver getDriver() {
+		return this.webDriver;
+	}
 
-    private WebDriver setWebDriver() {
-        String browser = EvnConfig.getBrowser();
-        String gridHub = EvnConfig.getGridHub();
-        if (gridHub.isEmpty()) {
-            // Initialize a local WebDriver instance
-            switch (browser) {
-                case ("firefox"):
-                    WebDriverManager.firefoxdriver().setup();
-                    FirefoxOptions firefoxOptions = new FirefoxOptions();
-                    firefoxOptions.setCapability("marionette", true);
-                    webDriver = new FirefoxDriver(firefoxOptions);
-                    break;
-                case ("chromeHeadless"):
-                    WebDriverManager.chromedriver().setup();
-                    ChromeOptions chromeHeadless = new ChromeOptions();
-                    chromeHeadless.setHeadless(true);
-                    webDriver = new ChromeDriver(chromeHeadless);
-                    break;
-                case ("edge"):
-                    WebDriverManager.edgedriver().setup();
-                    webDriver = new EdgeDriver();
-                    break;
-                default:
-                    WebDriverManager.chromedriver().setup();
-                    ChromeOptions chrome = new ChromeOptions();
-                    webDriver = new ChromeDriver(chrome);
-                    break;
-            }
-        } else {
-            // Use Selenium Grid to run tests. browser name must be specified.
-            if (browser.isEmpty()) {
-                System.out.println("*************** The browser's name should be specified ***************");
-                System.exit(0);
-            }
-            else{
-                // current Grid setup only support chrome and firefox.
-                DesiredCapabilities capabilities;
-                if ("firefox".equals(browser)) {
-                    capabilities = DesiredCapabilities.firefox();
-                    capabilities.setCapability("marionette", true);
-                    capabilities.setBrowserName("firefox");
-                } else {
-                    capabilities = DesiredCapabilities.chrome();
-                    capabilities.setBrowserName("chrome");
-                }
-                capabilities.setPlatform(Platform.LINUX);
-                try {
-                    webDriver = new RemoteWebDriver(new URL(gridHub), capabilities);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return webDriver;
-    }
+	private WebDriver setWebDriver() {
+		String browser = EvnConfig.getBrowser();
+		String gridHub = EvnConfig.getGridHub();
+		if (gridHub.isEmpty()) {
+			// Initialize a local WebDriver instance
+			switch (browser) {
+			case ("firefox"):
+				WebDriverManager.firefoxdriver().setup();
+				FirefoxOptions firefoxOptions = new FirefoxOptions();
+				firefoxOptions.setCapability("marionette", true);
+				webDriver = new FirefoxDriver(firefoxOptions);
+				break;
+			case ("chromeHeadless"):
+				WebDriverManager.chromedriver().setup();
+				ChromeOptions chromeHeadless = new ChromeOptions();
+				chromeHeadless.setHeadless(true);
+				webDriver = new ChromeDriver(chromeHeadless);
+				break;
+			case ("edge"):
+				WebDriverManager.edgedriver().setup();
+				webDriver = new EdgeDriver();
+				break;
+			default:
+				WebDriverManager.chromedriver().setup();
+				ChromeOptions chrome = new ChromeOptions();
+				webDriver = new ChromeDriver(chrome);
+				break;
+			}
+		} else {
+			// Use Selenium Grid to run tests. browser name must be specified.
+			if (browser.isEmpty()) {
+				System.out.println("*************** The browser's name should be specified ***************");
+				System.exit(0);
+			} else {
+				// current Grid setup only support chrome and firefox.
+				DesiredCapabilities capabilities;
+				if ("firefox".equals(browser)) {
+					capabilities = DesiredCapabilities.firefox();
+					capabilities.setCapability("marionette", true);
+					capabilities.setBrowserName("firefox");
+				} else {
+					capabilities = DesiredCapabilities.chrome();
+					capabilities.setBrowserName("chrome");
+				}
+				capabilities.setPlatform(Platform.LINUX);
+				try {
+					webDriver = new RemoteWebDriver(new URL(gridHub), capabilities);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return webDriver;
+	}
+
+	public String getBrowser() {
+
+		Capabilities cap = ((RemoteWebDriver) webDriver).getCapabilities();
+		String browserName = cap.getBrowserName().toLowerCase();
+
+		return browserName;
+
+	}
+
+	public void removeDriver() {
+		this.webDriver.quit();
+		;
+	}
+
 }
--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,44 +1,37 @@
 package test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-import com.aventstack.extentreports.ExtentTest;
-
 import core.driver.WebDriverFactory;
-import core.report.ExtentManager;
-import core.report.Report;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import projectconst.Constant;
 
+import core.report.Report;
+
+import projectconst.Constant;
 
 public class BaseTest {
 	WebDriver WEBDRIVER;
 	WebDriverFactory webDriverFactory;
 	Report report;
+
 	@BeforeTest
 	public void init() throws IOException {
-//		WebDriverManager.chromedriver().browserVersion(null).setup();
-//		WEBDRIVER = new ChromeDriver();
-//		WEBDRIVER.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-//		WEBDRIVER.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		webDriverFactory = new WebDriverFactory();
-        webDriverFactory.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        webDriverFactory.getDriver().manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-        webDriverFactory.getDriver().manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
-        webDriverFactory.getDriver().manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        webDriverFactory.getDriver().manage().window().maximize();
-		
-		// report = new Report (Constant.reportPath);
-		report = new Report(Constant.reportPath);
+		webDriverFactory.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+		webDriverFactory.getDriver().manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
+		webDriverFactory.getDriver().manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
+		webDriverFactory.getDriver().manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		webDriverFactory.getDriver().manage().window().maximize();
+		// String browser = webDriverFactory.getDriver().
+		report = new Report(Constant.reportPath + File.separator + "TestResult_" + webDriverFactory.getBrowser() + ".html");
 		webDriverFactory.getDriver().get(Constant.URL);
-		report.logInfo(webDriverFactory.getDriver(),Constant.captureImageFolder, "Start browser!");
+		report.logInfo(webDriverFactory.getDriver(), Constant.captureImageFolder, "Start browser!");
 		// config log
 		DOMConfigurator.configure("log4j.xml");
 
@@ -46,7 +39,7 @@ public class BaseTest {
 
 	@AfterTest
 	public void afterMethod() {
-		WEBDRIVER.quit();
+		webDriverFactory.removeDriver();
 		Report.saveReport();
 	}
 
